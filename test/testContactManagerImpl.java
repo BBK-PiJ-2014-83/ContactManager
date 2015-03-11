866e4721458d1bdd9a43da8b861bea724b41c1c4
John <jspears623@gmail.com>
2015-03-11 17:55:49 +0000

    Building up test for ContactManagerImpl

--- test/testContactManagerImpl.java
+++ test/testContactManagerImpl.java
@@ -5,5 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class testContactManagerImpl {
+    ContactManagerImpl testContactManager;
+    @Before
+    public void before() {
+        testContactManager = new ContactManagerImpl();
+    }
+
 
 }
